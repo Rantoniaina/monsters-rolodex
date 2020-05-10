474661edcafd9264ca9f9f68eb390d824f855d29
Rantoniaina <rantoniaina@hairun-technology.com>
2020-05-10 12:17:06 +0300

    Updates

---
+++ precache-manifest.59f5d8a835ac8cbf4be780316b0ed4a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de3ea46e7451cfbfcab10e006b090b4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f686d178f33576b48826",
+    "url": "/monsters-rolodex/static/css/main.31faeff6.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f686d178f33576b48826",
+    "url": "/monsters-rolodex/static/js/main.94f34a63.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
